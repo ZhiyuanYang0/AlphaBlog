6d51bac6ea635f9760921edf01cb1c963cbcfa08
Zhiyuan Yang <zhiyuanyang3@gmail.com>
2016-03-11 04:21:21 +0000

    Add users listing and update show article view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,40 +1,44 @@
 class UsersController < ApplicationController
+  
+  def index
+    @users = User.all
+  end
 
-    def new
-      @user = User.new   
-    end
+  def new
+    @user = User.new   
+  end
+  
+  def create
+    @user = User.new(user_params)
     
-    def create
-      @user = User.new(user_params)
-      
-      if @user.save
-        flash[:success] = "Welcome to the Starfall Blog #{@user.username}"
-        redirect_to articles_path
-      else
-        render 'new'
-      end
+    if @user.save
+      flash[:success] = "Welcome to the Starfall Blog #{@user.username}"
+      redirect_to articles_path
+    else
+      render 'new'
     end
-    
-    def edit
-        @user = User.find(params[:id])
-    end
-    
-    def update
-        @user = User.find(params[:id])
-        if @user.update(user_params)
-            flash[:success] = "Your account was updated successfully"
-            redirect_to articles_path
-        else
-            render 'edit'
-        end
-    end
-    
-    def show
+  end
+  
+  def edit
       @user = User.find(params[:id])
-    end
+  end
+  
+  def update
+      @user = User.find(params[:id])
+      if @user.update(user_params)
+          flash[:success] = "Your account was updated successfully"
+          redirect_to articles_path
+      else
+          render 'edit'
+      end
+  end
+  
+  def show
+    @user = User.find(params[:id])
+  end
 
-    private
-    def user_params
-      params.require(:user).permit(:username, :email, :password)
-    end
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
