474bd3a326f4bdb1d0e965c12f6c4ef098c92797
Zhiyuan Yang <zhiyuanyang3@gmail.com>
2016-03-12 10:33:46 +0000

    add config for text editor

--- config/application.rb
+++ config/application.rb
@@ -6,10 +6,6 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config.assets.precompile += Ckeditor.assets
-config.assets.precompile += %w( ckeditor/* )
-config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
-
 module AlphaBlog
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
@@ -26,5 +22,10 @@ module AlphaBlog
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    
+    config.assets.enabled = true
+    config.assets.precompile += Ckeditor.assets
+    config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
+
   end
 end
